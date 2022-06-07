63a18077b6fc31b1124e05ea5bcbbe6927d920e0
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-07 14:51:02 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+0-putchar.c char text[9] = "_putchar";
+0-putchar.c int i = 0;
+
+0-putchar.c for (i = 0; i < 8; i++)
+0-putchar.c {
+0-putchar.c 0-putchar.c _putchar(text[i]);
+0-putchar.c }
+0-putchar.c _putchar(n);
+
+0-putchar.c return (0);
+}
