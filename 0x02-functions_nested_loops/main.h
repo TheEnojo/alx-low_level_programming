54305f934c6ef5f37e5bbacfb212632aabbb378c
TheEnojo <105020842+TheEnojo@users.noreply.github.com>
2022-06-09 00:47:41 +0100

    Main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,61 +1,13 @@
-#ifndef _MAIN_H_
-
-#define _MAIN_H_
-
-/**
-
-*putchar - writes the character c to stdout
-
-*@c: The character to print
-
-*
-
-*Return: On success 1.
-
-*On error, -1 is returned, and error is set appropriately
-
-*/
-
-int _putchar(char c);
-
-/**                                                                                                                                              
-
- *print_alphabet - print lowercase alphabet                                                                                                                               
-
- */
-
+int _putchar(int x);
 void print_alphabet(void);
-
-/**
-
- * print_alphabet_x10 - print lowercase alphabet ten times
-
- */
-
 void print_alphabet_x10(void);
-
-/**
-
- * checks for lowercase character.
-
- */
-
 int _islower(int c);
-
-/**
-
- * checks for alphabetic character.
-
- */
-
 int _isalpha(int c);
-
-/**
-
- *function that prints the sign of a number.
-
- */
-
 int print_sign(int n);
-
-#endif
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
