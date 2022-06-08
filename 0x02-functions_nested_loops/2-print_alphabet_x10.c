21c8613600d818872ecc60a39922d8815db087a2
TheEnojo <105020842+TheEnojo@users.noreply.github.com>
2022-06-08 05:20:30 +0100

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,18 @@
 */
 void print_alphabet_x10(void)
 {
-char alpha;
+	char alpha;
 
-int count;
+	int count;
 
-for (count = 1 ; count <= 10 ; count++)
-{
-for (alpha = a ; alpha <= z ; alpha++)
-{
-_putchar(alpha);
-}
-_putchar(n);
-}
+	for (count = 1 ; count <= 10 ; count++)
+	{
+		for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
 
 
 }
