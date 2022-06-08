9b6cfb3d29ab95eb2905071e9d6d5ffc58cf7036
TheEnojo <105020842+TheEnojo@users.noreply.github.com>
2022-06-09 00:40:53 +0100

    New file

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
- * Return: Always return 0.
+ * Return: Always 0.
  */
 int main(void)
 {
-	print_alphabet();
+	jack_bauer();
 	return (0);
 }
