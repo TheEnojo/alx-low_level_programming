42a663aadb7765b6438d3d3127a9333756e1070d
TheEnojo <105020842+TheEnojo@users.noreply.github.com>
2022-06-09 01:08:39 +0100

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-int i = 0;
-long int a = 0, b = 1, next;
+	int i = 0;
+	long int a = 0, b = 1, next;
 
-while (i < 50)
-{
-next = a + b;
-a = b;
-b = next;
-printf("%lu", next);
-if (i < 49)
-{
-printf(", ");
-}
-i++;
-}
-putchar(n);
-return (0);
+	while (i < 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
