758f8b5ac67f3a8ea8e43dbb0e8d3ab954ba2b94
TheEnojo <105020842+TheEnojo@users.noreply.github.com>
2022-06-11 01:37:53 +0100

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,15 +10,15 @@
 
 void print_number(int n)
 {
-if (n < 0)
-{
-_putchar(-);
-n = 0 - n;
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = 0 - n;
+	}
 
-if (n >= 10)
-{
-print_number(n / 10);
-}
-_putchar((n % 10) + 0);
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
 }
