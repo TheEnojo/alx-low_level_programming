a1c43b7dcdfcc6b7ec3e3674ee784c51107911bd
TheEnojo <105020842+TheEnojo@users.noreply.github.com>
2022-06-11 01:17:09 +0100

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@
  */
 void print_most_numbers(void)
 {
-int num;
+	int num;
 
-for (num = 0; num < 10; num++)
-{
-if (num != 2 && num != 4)
-_putchar(num + 0);
-}
-_putchar(n);
+	for (num = 0; num < 10; num++)
+	{
+		if (num != 2 && num != 4)
+			_putchar(num + '0');
+	}
+	_putchar('\n');
 }
