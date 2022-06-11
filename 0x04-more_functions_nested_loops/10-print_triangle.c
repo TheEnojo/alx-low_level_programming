1ee12ae77b45127d6fe02ae8c3d636e195a8f4c9
TheEnojo <105020842+TheEnojo@users.noreply.github.com>
2022-06-11 01:32:02 +0100

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,22 +9,22 @@
  */
 void print_triangle(int size)
 {
-int width;
-int len;
-int incr;
+	int width;
+	int len;
+	int incr;
 
-if (size <= 0)
-_putchar(n);
-for (width = 0; width < size; width++)
-{
-for (len = size - width; len > 1; len--)
-{
-_putchar( );
-}
-for (incr = width + len; incr >= 1; incr--)
-{
-_putchar(#);
-}
-_putchar(n);
-}
+	if (size <= 0)
+		_putchar('\n');
+	for (width = 0; width < size; width++)
+	{
+		for (len = size - width; len > 1; len--)
+		{
+			_putchar(' ');
+		}
+		for (incr = width + len; incr >= 1; incr--)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
