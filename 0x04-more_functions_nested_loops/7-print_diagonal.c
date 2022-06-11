490b60a45712b8ae585484b080cfb1d0222f1fef
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-10 17:24:19 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Function entry point
+ * @n: param
+ * Desc: a function that draws a diagonal line on the terminal
+ *
+ * Return: Void
+ */
+void print_diagonal(int n)
+{
+int incr;
+int gap;
+
+if (n <= 0)
+_putchar(n);
+for (incr = 0; incr < n; incr++)
+{
+for (gap = 0; gap < incr; gap++)
+{
+_putchar( );
+}
+_putchar(\);
+_putchar(n);
+}
+}
