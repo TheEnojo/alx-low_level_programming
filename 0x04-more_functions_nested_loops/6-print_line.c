12c8725e75031a608385bd98fac54653cfd8a3f6
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-10 17:22:09 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ * @n: function param
+ * Desc: draws a straight line in the termnal
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+int i;
+
+i = 0;
+while (i < n)
+{
+_putchar(_);
+i++;
+}
+_putchar(n);
+}
