cebc69d0c94d6d0bf69459405a6f533872044fbe
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-10 17:29:18 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desc: a FizzBuzz program that prints from 1 to 100
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 1; num <= 100; num++)
+{
+if (num % 15 == 0)
+printf("FizzBuzz ");
+else if (num % 3 == 0)
+printf("Fizz ");
+else if (num % 5 == 0)
+if (num == 100)
+printf("Buzz");
+else
+printf("Buzz ");
+else
+printf("%d ", num);
+}
+printf("\n");
+
+return (0);
+}
