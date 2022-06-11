1f3b88fd942cc4c84ca39e26962d8292284b79ff
TheEnojo <105020842+TheEnojo@users.noreply.github.com>
2022-06-11 01:14:39 +0100

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
  */
 void print_numbers(void)
 {
-int num;
+	int num;
 
-for (num = 0; num < 10; num++)
-{
-_putchar(num + 0);
-}
-_putchar(n);
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
 }
