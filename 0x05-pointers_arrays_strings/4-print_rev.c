b29bb9dbc0deb7d1695e4e09b3d81bdb201d618d
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-14 17:10:39 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - function entry point
+ * @s: function param
+ * Desc: prints a string in reverse
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int x = 0;
+int y;
+
+while (s[x] != 0)
+{
+x++;
+}
+
+for (y = x - 1; y >= 0; y--)
+{
+_putchar(s[y]);
+}
+_putchar(n);
+}
