e7c9b1cb7a2d694bdf4026d8fe813c1d2c784f7e
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-14 17:06:06 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - function entry point
+ * @a: function param
+ * @b: function param
+ * Desc: a function that swaps the values of two integers
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
