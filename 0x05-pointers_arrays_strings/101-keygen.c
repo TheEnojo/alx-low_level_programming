0ba3ef5c6a709c6cde3476d91c9c973e7a5a2d78
Enojo Godwin <105020842+TheEnojo@users.noreply.github.com>
2022-06-15 01:38:20 +0100

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,26 +9,26 @@
  */
 int main(void)
 {
-int pass[100];
-int i, count, s;
+	int pass[100];
+	int i, count, s;
 
-count = 0;
+	count = 0;
 
-srand(time(NULL));
+	srand(time(NULL));
 
-for (i = 0; i < 100; i++)
-{
-pass[i] = rand() % 78;
-count += (pass[i] + 0);
-putchar(pass[i] + 0);
-if ((2772 - count) - 0 < 78)
-{
-s = 2772 - count - 0;
-count += s;
-putchar(s + 0);
-break;
-}
-}
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		count += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - count) - '0' < 78)
+		{
+			s = 2772 - count - '0';
+			count += s;
+			putchar(s + '0');
+			break;
+		}
+	}
 
-return (0);
+	return (0);
 }
