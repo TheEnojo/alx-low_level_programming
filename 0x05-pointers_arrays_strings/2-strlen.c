71f3475577efed7f67c831f47eb6c95512eeb1da
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-14 17:07:55 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function entry point
+ * @s: function param
+ * Desc: a function that returns the len of a string
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s != 0)
+{
+s++;
+len++;
+}
+
+return (len);
+}
