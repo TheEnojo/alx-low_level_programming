c64c992a3b0ab52b72948f850e475257f668c5cc
Enojo Godwin <105020842+TheEnojo@users.noreply.github.com>
2022-06-15 01:29:19 +0100

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,23 +8,23 @@
  */
 void puts_half(char *str)
 {
-int len = 0;
-int x;
-int n;
+	int len = 0;
+	int x;
+	int n;
 
-while (str[len] != 0)
-{
-len++;
-}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-if (len % 2 != 0)
-n = (len + 1) / 2;
-else
-n = len / 2;
+	if (len % 2 != 0)
+		n = (len + 1) / 2;
+	else
+		n = len / 2;
 
-for (x = n; x < len; x++)
-{
-_putchar(str[x]);
-}
-_putchar(n);
+	for (x = n; x < len; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
 }
