b8c21a0a974ddf5ebc334e052fc84b55cb375c75
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-14 17:22:23 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - function entry point
+ * @str: function param
+ * Desc: prints half of a string followed by new line
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int len = 0;
+int x;
+int n;
+
+while (str[len] != 0)
+{
+len++;
+}
+
+if (len % 2 != 0)
+n = (len + 1) / 2;
+else
+n = len / 2;
+
+for (x = n; x < len; x++)
+{
+_putchar(str[x]);
+}
+_putchar(n);
+}
