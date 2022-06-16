febddfb4c319f594c6c8744e7784e18c50aa3e89
Enojo Godwin <105020842+TheEnojo@users.noreply.github.com>
2022-06-17 00:50:11 +0100

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,28 +9,28 @@
  */
 char *_strcat(char *dest, char *src)
 {
-0-strcat.c int len_d = 0;
-0-strcat.c int len_s = 0;
-0-strcat.c int x = 0;
-0-strcat.c int y;
+	int len_d = 0;
+	int len_s = 0;
+	int x = 0;
+	int y;
 
-0-strcat.c while (dest[x])
-0-strcat.c {
-0-strcat.c 0-strcat.c len_d++;
-0-strcat.c 0-strcat.c x++;
-0-strcat.c }
+	while (dest[x])
+	{
+		len_d++;
+		x++;
+	}
 
-0-strcat.c x = 0;
-0-strcat.c while (src[x])
-0-strcat.c {
-0-strcat.c 0-strcat.c len_s++;
-0-strcat.c 0-strcat.c x++;
-0-strcat.c }
+	x = 0;
+	while (src[x])
+	{
+		len_s++;
+		x++;
+	}
 
-0-strcat.c for (y = 0; y <= len_s; y++)
-0-strcat.c 0-strcat.c dest[len_d + y] = src[y];
+	for (y = 0; y <= len_s; y++)
+		dest[len_d + y] = src[y];
 
-0-strcat.c dest[len_d + y] = 0;
+	dest[len_d + y] = '\0';
 
-0-strcat.c return (dest);
+	return (dest);
 }
