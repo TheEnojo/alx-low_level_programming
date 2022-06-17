b56c6f67b43ca1c4ff65f47f8735def1bc9dab70
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-16 17:11:51 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - function entry point
+ * @s1: first string
+ * @s2: second string
+ * Desc: compares two strings
+ * Return: Always 0 if strings match, else a non-zero value
+ */
+int _strcmp(char *s1, char *s2)
+{
+int x = 0;
+
+while (s1[x] && s2[x])
+{
+if (s1[x] != s2[x])
+{
+return (s1[x] - s2[x]);
+}
+x++;
+}
+
+return (0);
+}
