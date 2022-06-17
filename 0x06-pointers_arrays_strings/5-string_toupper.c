8555ac137314f0ae867da7e1571bb34f8b10ea4f
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-16 17:19:03 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * string_toupper - function entry point
+ * @s: pointer variable
+ * Desc: changes all lowercase letters of a string to uppercase
+ * Return: string array
+ */
+char *string_toupper(char *s)
+{
+int x = 0;
+int len = 0;
+
+while (s[x])
+{
+len++;
+x++;
+}
+
+for (x = 0; x <= len; x++)
+{
+if (s[x] > 96 && s[x] < 123)
+{
+s[x] = s[x] - 32;
+}
+}
+
+return (s);
+}
