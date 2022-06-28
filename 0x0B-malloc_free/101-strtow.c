fe8df49c53c31d0572d1959b77ea977b6c2fcc49
Enojo Godwin <105020842+TheEnojo@users.noreply.github.com>
2022-06-29 00:47:30 +0100

    completed

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,9 +13,9 @@ int i, n = 0;
 
 for (i = 0; s[i]; i++)
 {
-if (s[i] ==  )
+if (s[i] == ' ')
 {
-if (s[i + 1] !=   && s[i + 1] != 0)
+if (s[i + 1] != ' ' && s[i + 1] != '\0')
 n++;
 }
 else if (i == 0)
@@ -35,7 +35,7 @@ char **strtow(char *str)
 int i, j, k, l, n = 0, wc = 0;
 char **w;
 
-if (str == NULL || *str == 0)
+if (str == NULL || *str == '\0')
 return (NULL);
 n = wrdcnt(str);
 if (n == 1)
@@ -47,9 +47,9 @@ w[n - 1] = NULL;
 i = 0;
 while (str[i])
 {
-if (str[i] !=   && (i == 0 || str[i - 1] ==  ))
+if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 {
-for (j = 1; str[i + j] !=   && str[i + j]; j++)
+for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 ;
 j++;
 w[wc] = (char *)malloc(j *sizeof(char));
@@ -64,7 +64,7 @@ return (NULL);
 }
 for (l = 0; l < j; l++)
 w[wc][l] = str[i + l];
-w[wc][l] = 0;
+w[wc][l] = '\0';
 wc++;
 i += j;
 }
