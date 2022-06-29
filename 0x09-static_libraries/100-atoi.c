9ce0f329b8d1982deb6ff4df05c4efdfd0181a93
Grace Enojo Godwin <nhojoh17@gmail.com>
2022-06-29 13:21:47 -0700

    completed

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - function entry point
+ * @s: function param
+ * Desc: converts a string to an integer
+ * Return: Always 0
+ */
+int _atoi(char *s)
+{
+int x = 0;
+int y;
+unsigned int num = 0;
+
+while (s[x] != 0)
+{
+x++;
+}
+for (y = 0; y <= x; y++)
+{
+if (s[y] == 59)
+{
+break;
+}
+if (s[y] > 47 && s[y] < 58)
+{
+num = num * 10 + s[y] - 48;
+}
+}
+for (y = 0; y <= x; y++)
+{
+if (s[y] == 59)
+break;
+if (s[y] == 45)
+num = 0 - num;
+}
+
+return (num);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - function entry point
+ * @dest: function param
+ * @src: function param
+ * Desc: copies the string pointed to by src
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len = 0;
+int x;
+
+while (src[len])
+{
+len++;
+}
+for (x = 0; x <= len; x++)
+{
+dest[x] = src[x];
+}
+
+return (dest);
+}
